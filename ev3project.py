55eeef0a064ea51cb2db9f7941cecf9bdcd0ea40
Alan Smith <alan@randomsmiths.com>
2016-12-24 08:00:10 -0500

    Splitting out Commit and Changeset classes into their own file

--- ev3project.py
+++ ev3project.py
@@ -1,151 +1,11 @@
-from mako.template import Template
 import json
-import zipfile
-import cStringIO
-import hashlib
 import os
 import time
 import sys
-import xml.etree.ElementTree as ET
 import argparse
 from operator import methodcaller
-
-class Commit(object):
-    def __init__(self, commit_id, commitDetails):
-      self.commit_id = commit_id;
-      self.commitDetails = commitDetails;
-    def cid(self):
-      return self.commit_id;
-    def files(self):
-      return self.commitDetails["files"];  
-    def getSHA(self, filename):
-      try:
-          return self.commitDetails["files"][filename]
-      except:
-          return ""      
-    def remove_file(self, filename):
-      if filename in commitDetails["files"]:
-          del commitDetails["files"][filename];
-    def parent(self):
-      return self.commitDetails["parent"];
-    def mergedfrom(self):
-      return self.commitDetails["mergedFrom"];
-    def setParent(self, parent):
-      self.commitDetails["parent"] = parent;
-    def time(self):
-      return self.commitDetails["time"];
-    def timeStr(self):
-      return time.strftime("%a %b %d, %Y %I:%M %p %Z", time.localtime(self.time()));
-    def name(self):
-      return self.commitDetails["name"];
-    def host(self):
-      return self.commitDetails["host"];
-    def comment(self):
-      return self.commitDetails["comment"];
-      
-    def get_EV3_data(self, projName, fullpath):
-        variables = {}
-        programs = []
-        myblockdefs = []
-        medias = []
-
-        ev3Contents = cStringIO.StringIO()
-        
-        ev3_template = Template(filename='HTMLTemplates/lvprojx.html');
-        
-        with zipfile.ZipFile(ev3Contents, "a", zipfile.ZIP_DEFLATED, False) as zf:
-            # so we can keep track of the commit this is from
-            zf.writestr("ev3hub.json", '{{"fromCommit": "{0}","project":"{1}"}}'.format(self.cid(), projName))
-            for filename in self.files():
-                if self.files()[filename] == "":
-                    varInfo = filename.split(':', 1);
-                    variables[varInfo[1]] = varInfo[0]
-                else:
-                    with open(fullpath + self.files()[filename], 'r') as file:
-                        zf.writestr(filename, file.read())    
-                    file_parts = filename.split('.',1);
-     
-                    basename = file_parts[0];
-                    ext = file_parts[-1];
-                    
-                    if ext.startswith('ev3p'):
-                        if ext == 'ev3p.mbxml':
-                            myblockdefs.append(basename + '.ev3p')
-                        else:
-                          programs.append(filename)
-                    elif ext in ['rgf', 'rsf', 'rtf']:
-                        medias.append(filename)
-
-            for myblock in myblockdefs:
-                programs.remove(myblock)
-                
-            # generate lvprojx.proj file here
-            lvprojx_data = ev3_template.render(programs= sorted(programs, key=unicode.lower), 
-                                               myblockdefs= sorted(myblockdefs, key=unicode.lower),
-                                               vars= variables,
-                                               medias= sorted(medias, key=unicode.lower),
-                                               daisychain= False,
-                                               strict_underfined=True)                
-            zf.writestr('Project.lvprojx', lvprojx_data)
-            
-            for zfile in zf.filelist:
-                zfile.create_system = 0
-            zf.close()
-        return ev3Contents.getvalue();
+from ev3commit import Commit, Changeset
        
-    def __str__(self):
-      return "{0}:{1}".format(self.commit_id, self.commitDetails)
-    @classmethod
-    def from_file(cls, cid, file):
-      return cls(cid, json.loads(file.read()))  
-    
-    @classmethod
-    def from_id(cls, cid, path):
-      if "{0}".format(cid) == "0":
-          commitDetails = {"parent" : 0, "mergedFrom" : 0, "time" : time.time(), "name" : "", "host" : "", "comment" : "", "files" : {}}
-          return cls(cid, commitDetails)
-      return cls.from_file(cid, open(cls.file_from_id(path, cid), "r"))
-      
-    @classmethod
-    def file_from_id(cls, path, cid):
-      return os.path.join(path, "{0}.json".format(cid));
-          
-class Changeset(object):
-    def __str__(self):
-      return "Removed: {0}\nNew: {1}\nModified: {2}\n".format(self.m_removedFiles, self.m_newFiles, self.m_modifiedFiles)
-    
-    def __init__(self, oldCommit, newCommit):
-        self.m_removedFiles = [];
-        self.m_newFiles = [];
-        self.m_modifiedFiles = [];
-        
-        oldCommitFiles = oldCommit.files();
-        newCommitFiles = newCommit.files();
-        
-        for newF in newCommitFiles:
-           new_filename = newF[0];
-           if newF in oldCommitFiles:
-               if(newCommitFiles[newF] != oldCommitFiles[newF]):
-                   self.m_modifiedFiles.append(newF);
-           else:
-              self.m_newFiles.append(newF);    
-        
-        for oldF in oldCommitFiles:
-            if oldF not in newCommitFiles:
-                self.m_removedFiles.append(oldF);     
-    def different(self):
-        if (self.m_removedFiles == []) and (self.m_newFiles == []) and (self.m_modifiedFiles == []):
-            return False
-        else:
-            return True
-    def removedFiles(self):
-        return self.m_removedFiles;
-    def newFiles(self):
-        return self.m_newFiles;
-    def modifiedFiles(self):
-        return self.m_modifiedFiles;        
-
-
 class EV3Project(object):
     @classmethod
     def newProject(cls, user, name, ev3data, who, host):
@@ -202,13 +62,10 @@ class EV3Project(object):
         return str(cid);  
     
     def download(self, cid):
+        commit_id = cid;
         if cid == "head":
-            return self.getEV3Data(Commit.from_id(self.head, self.path))
-        else:
-            return self.getEV3Data(Commit.from_id(cid, self.path))
-            
-    def getEV3Data(self, commit):
-        return commit.get_EV3_data(self.name, self.fullpath("repo/"))
+            commit_id = self.head;
+        return Commit.from_id(commit_id, self.path).get_EV3_data(self.name, self.fullpath("repo/"))
         
     def uploadCommit(self, ev3data, comment, who, host):
         files = {};
