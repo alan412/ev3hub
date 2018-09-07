85b8647e8ad1ace4446061b73b5206ba295a346b
Alan Smith <alan@randomsmiths.com>
2018-09-07 15:01:04 -0400

    add additional tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -12,64 +12,78 @@ class SimpleCPTest(helper.CPWebCase):
     def setup_server():
         cherrypy.tree.mount(EV3hub(), '/', 'development.conf')
 
+    def getPageWithPatch(self, page, headers=None, method='GET', body=None, expectedStatus=200):
+        sess_mock = RamSession()
+        sess_mock['username'] = 'fll447'
+        sess_mock['project'] = 'AnimalAllies'
+        with patch('cherrypy.session', sess_mock, create=True):
+            self.getPage(page, headers, method, body)
+            self.assertStatus(expectedStatus)
+
+    def sendForm(self, page, body, expectedStatus=200):
+        headers = [
+            ('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8'),
+            ('Content-length', str(len(body)))]
+        self.getPageWithPatch(page, headers, 'POST', body, expectedStatus)
+
     def test_login(self):
         self.getPage("/")
         self.assertStatus('200 OK')
 
     def test_mainpage(self):
-        sess_mock = RamSession()
-        sess_mock['username'] = 'fll447'
-        sess_mock['project'] = 'AnimalAllies'
-        with patch('cherrypy.session', sess_mock, create=True):
-            self.getPage("/")
-        self.assertStatus('200 OK')
+        self.getPageWithPatch("/")
 
     def test_detailspage(self):
-        sess_mock = RamSession()
-        sess_mock['username'] = 'fll447'
-        sess_mock['project'] = 'AnimalAllies'
-        with patch('cherrypy.session', sess_mock, create=True):
-            self.getPage("/")
-            headers = [
-                ('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8'),
-                ('Content-length', '5')]
-            body = 'cid=6'
-            self.getPage("/details", headers, 'POST', body)
-            self.assertStatus('200 OK')
-            self.getPage("/graph", headers, 'POST', body)
+        self.sendForm("/details", 'cid=6')
+        self.sendForm("/graph", 'cid=6')
+        self.sendForm("/graphPage", 'cid=6')
 
     def test_projectList(self):
-        sess_mock = RamSession()
-        sess_mock['username'] = 'fll447'
-        sess_mock['project'] = 'AnimalAllies'
-        with patch('cherrypy.session', sess_mock, create=True):
-            self.getPage("/projects")
-        self.assertStatus('200 OK')
+        self.getPageWithPatch("/projects")
 
     def test_logout(self):
-        sess_mock = RamSession()
-        sess_mock['username'] = 'fll447'
-        sess_mock['project'] = 'AnimalAllies'
-        with patch('cherrypy.session', sess_mock, create=True):
-            self.getPage("/logout")
-        self.assertStatus('200 OK')
+        self.getPageWithPatch("/logout")
 
     def test_showUpload(self):
-        sess_mock = RamSession()
-        sess_mock['username'] = 'fll447'
-        sess_mock['project'] = 'AnimalAllies'
-        with patch('cherrypy.session', sess_mock, create=True):
-            self.getPage("/upload")
-        self.assertStatus('200 OK')
+        self.getPageWithPatch("/upload")
+
+    def test_getTags(self):
+        self.getPageWithPatch("/tags")
+
+    def test_add_and_del_Tags(self):
+        self.sendForm("/createTag", 'cid=6&description=test')
+        self.sendForm("/removeTag", 'tag=test')
 
     def test_diff(self):
-        sess_mock = RamSession()
-        sess_mock['username'] = 'fll447'
-        sess_mock['project'] = 'AnimalAllies'
-        with patch('cherrypy.session', sess_mock, create=True):
-            headers = [
-                ('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8'),
-                ('Content-length', '13')]
-            body = 'cid1=6&cid2=5'
-            self.getPage("/diff", headers, 'POST', body)
-        self.assertStatus('200 OK')
+        self.sendForm("/diff", 'cid1=6&cid2=5')
+
+    def test_change_to_bogus_project(self):
+        self.sendForm("/changeProject", 'project=BogusProject')
+
+    def test_change_project(self):
+        self.sendForm("/changeProject", 'project=AnimalAllies',
+                      expectedStatus=303)
+
+    def test_download(self):
+        self.sendForm("/download", 'cid=head')
+        self.sendForm("/download", 'cid=6')
+
+    def test_changeProject_not_logged_in(self):
+        body = "project=FakeProject"
+        headers = [
+            ('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8'),
+            ('Content-length', str(len(body)))]
+        self.getPage('/changeProject', headers, 'POST', body)
+        self.assertStatus(200)
+
+    def test_logins(self):
+        self.sendForm("/login", 'username=fll447&password=test')
+        self.sendForm("/login", 'username=fll447&password=badPassword')
+
+    def test_createUser(self):
+        self.sendForm(
+            "/createUser", 'username=fred&email=asd&password=pass&password2=fail')
+        self.sendForm(
+            "/createUser", 'username=fll447&email=asd&password=pass&password2=pass')
+        self.sendForm(
+            "/createUser", 'username=fred&email=asd&password=pass&password2=pass')
