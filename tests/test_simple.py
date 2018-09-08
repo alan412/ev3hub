9b9d013c76940baa59d9afcae8548539f077c1c6
Alan Smith <alan@randomsmiths.com>
2018-09-07 21:28:52 -0400

    additional tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,3 +1,5 @@
+import base64
+
 from unittest.mock import patch
 
 import cherrypy
@@ -11,6 +13,11 @@ class SimpleCPTest(helper.CPWebCase):
     @staticmethod
     def setup_server():
         cherrypy.tree.mount(EV3hub(), '/', 'development.conf')
+
+# note to self - look at pytest-datafiles
+    def load_file(self, filename):
+        with open(filename, 'rb') as f:
+            return base64.b64encode(f.read())
 
     def getPageWithPatch(self, page, headers=None, method='GET', body=None, expectedStatus=200):
         sess_mock = RamSession()
@@ -46,6 +53,8 @@ class SimpleCPTest(helper.CPWebCase):
 
     def test_showUpload(self):
         self.getPageWithPatch("/upload")
+        self.getPage('/upload')   # without session data
+        self.assertStatus('200 OK')
 
     def test_getTags(self):
         self.getPageWithPatch("/tags")
@@ -103,3 +112,55 @@ class SimpleCPTest(helper.CPWebCase):
 
     def test_forgot(self):
         self.sendForm('/forgot', 'username=fll447&token=ABCD')
+
+    def test_rename_project(self):
+        # rename (and back)
+        self.sendForm('/renameProject', 'project=AnimalAllies&newName=tmpProj')
+        self.sendForm('/renameProject', 'project=tmpProj&newName=AnimalAllies')
+
+        # rename bad one
+        self.sendForm('/renameProject', 'project=bogus&newName=tmpProj')
+
+    def test_remove_and_restore_project(self):
+        # bad password
+        self.sendForm('/removeProject', 'project=AnimalAllies&password=foof')
+        # bad project
+        self.sendForm('/removeProject', 'project=bogus&password=kira')
+        # actual removal
+        self.sendForm('/removeProject', 'project=AnimalAllies&password=kira')
+
+        # restore (actual)
+        self.sendForm('/restoreProject', 'project=AnimalAllies')
+        # bad restore
+        self.sendForm('/restoreProject', 'project=bogus')
+
+    def test_update_settings(self):
+        self.sendForm(
+            '/updateSettings', 'email=alan@randomsmiths.com&newpw1=&newpw2=&password=bogus')
+        self.sendForm(
+            '/updateSettings',
+            'email=alan@randomsmiths.com&newpw1=right&newpw2=wrong&password=kira')
+        self.sendForm(
+            '/updateSettings', 'email=alan@randomsmiths.com&newpw1=kira&newpw2=kira&password=kira')
+        self.sendForm(
+            '/updateSettings', 'email=joe@randomsmiths.com&newpw1=kira&newpw2=kira&password=kira')
+        self.sendForm(
+            '/updateSettings', 'email=alan@randomsmiths.com&newpw1=kira&newpw2=kira&password=kira')
+
+    def test_new_project(self):
+        boundary = "---Impossible4394932"
+        body = ('--'+boundary + '\r\nContent-Disposition: form-data; name="project"\r\n\r\n'
+                'test12345\r\n' +
+                '--'+boundary + '\r\nContent-Disposition: form-data; name="who"\r\n\r\n'
+                'me\r\n' +
+                '--'+boundary + '\r\nContent-Disposition: form-data; name="host"\r\n\r\n'
+                '127.0.0.1\r\n--' + boundary +
+                '\r\nContent-Disposition: form-data; name="ev3file"; filename="newFile.ev3"\r\n' +
+                'Content-Type: application/octet-stream\r\n\r\n' +
+                str(self.load_file('data/newFile.ev3')) +
+                '\r\n--'+boundary+'--\r\n')
+
+        header = [('Content-type', 'multipart/form-data; boundary='+boundary),
+                  ('Content-Length', str(len(body)))]
+        print(header, '\n', body)
+        self.getPageWithPatch('/newProject', header, 'POST', body)
